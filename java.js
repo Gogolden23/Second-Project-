7df6ac75e1601c3fce629153cfb079144550f66c
Gogolden23 <rgolden89348@uat.edu>
2024-05-25 17:48:11 -0700

    Add files via upload

---
+++ java.js
@@ -0,0 +1,68 @@
+
+var x = 5; // integer
+var status = true; // boolean - TF
+var x = 6;
+var y = 4; 
+var z = x+y;
+console.log(z)
+x = 8 
+console.log(x)
+
+let first_name = "Ryan";
+let last_name = "Golden"
+console.log("My full name is", first_name, last_name);
+// const - can not redclare once constant!
+const pi = 3.14;
+console.log(pi)
+// Java has differnt types of operators
+// Arithmetic
+
+var r = 7; 
+var y = 12;
+console.log(r+y)
+console.log(r-y)
+console.log(r/y)
+console.log(r*y)
+console.log(r%y)
+
+// Assigment
+
+let w = 12;
+let t = 8;
+w += t; // w = w+t
+
+console.log(w+t)
+
+// compariosn 
+
+let num1 = 35;
+let num2 = 55;
+console.log(num1 > num2)
+console.log(num1 <= num2)
+console.log(num2 <= num1)
+console.log(num1 != num2)
+
+// logical ops and &, or | , not !
+
+let isadult  = true;
+
+let hasalisense = true;
+
+let candrive = isadult && hasalisense ;
+
+console.log(candrive)
+
+let nocar = false;
+let underage = true;
+
+// conditionals  if  ,  else  else -if 
+let age = 20;
+if(age >=18){
+    console.log("you are a adult")
+}
+
+
+
+
+
+
